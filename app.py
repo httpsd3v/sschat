a511b5fa95d0051700ba4bff972ee8f627f704da
Brad <perpj339@gmail.com>
2026-03-28 13:40:35 +0300

    Refactor chat application HTML template and styles

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ HTML_TEMPLATE = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>InstaChat Pro</title>
+    <title>ssChat Pro</title>
     <script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
     <style>
         body { 
